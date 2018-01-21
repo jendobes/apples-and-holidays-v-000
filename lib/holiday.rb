bd0e6d24692a597d6284aac283a52780ab114283
Jennifer Dobak <jdobak1@gmail.com>
2018-01-21 18:24:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       holiday << "Balloons"
+      binding.binding.pry
     end
   end
 end
